7c7f4e8e0543dcc8715663a8b2485f4b06178a40
root <root@picoctf>
2026-07-17 12:25:14 +0100

    feat: personal config, remove demo posts

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -2,14 +2,13 @@ import { defineAstroPaperConfig } from "./src/types/config";
 
 export default defineAstroPaperConfig({
   site: {
-    url: "https://astro-paper.pages.dev/",
-    title: "AstroPaper",
-    description: "A minimal, responsive and SEO-friendly Astro blog theme.",
-    author: "Sat Naing",
-    profile: "https://satna.ing",
+    url: "https://bourhanbouacida-github-io.pages.dev/",
+    title: "B0urh4n",
+    description: "Newbie today, but not tomorrow.",
+    author: "Bourhan Bouacida",
     ogImage: "default-og.jpg",
     lang: "en",
-    timezone: "Asia/Bangkok",
+    timezone: "Africa/Algiers",
     dir: "ltr",
   },
   posts: {
@@ -23,23 +22,22 @@ export default defineAstroPaperConfig({
     showArchives: true,
     showBackButton: true,
     editPost: {
-      enabled: true,
-      url: "https://github.com/satnaing/astro-paper/edit/main/",
+      enabled: false,
     },
     search: "pagefind",
   },
   socials: [
-    { name: "github",   url: "https://github.com/satnaing/astro-paper" },
-    { name: "x",        url: "https://x.com/username" },
-    { name: "linkedin", url: "https://www.linkedin.com/in/username/" },
-    { name: "mail",     url: "mailto:yourmail@gmail.com" },
+    { name: "github",   url: "https://github.com/BourhanBouacida" },
+    { name: "linkedin", url: "https://www.linkedin.com/in/bourhan-bouacida/" },
+    { name: "x",        url: "https://x.com/BourhanBouacida" },
+    { name: "facebook", url: "https://www.facebook.com/Bourhan.Bouacida.OFFICIEL1/" },
+    { name: "mail",     url: "mailto:goldenbourhane@gmail.com" },
   ],
   shareLinks: [
     { name: "whatsapp", url: "https://wa.me/?text=" },
     { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
     { name: "x",        url: "https://x.com/intent/post?url=" },
     { name: "telegram", url: "https://t.me/share/url?url=" },
-    { name: "pinterest", url: "https://pinterest.com/pin/create/button/?url=" },
     { name: "mail",     url: "mailto:?subject=See%20this%20post&body=" },
   ],
 });
